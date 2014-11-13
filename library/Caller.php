02af774aa34b108cd220eb6c5846c7695945c3fd
wzy <monkeydady@gmail.com>
2014-11-13 11:19:25 +0800

    caller部分实现

--- library/Caller.php
+++ library/Caller.php
@@ -5,6 +5,30 @@
  */
 class Caller
 {
+	private static $_calleesuffix;
+	private static $_calleepath;
+	private static $_calleelist;
+	
+	public static function setCalleesuffix($suffix)
+	{
+		self::$_calleesuffix = $suffix;
+	}
+	
+	public static function getCalleesuffix()
+	{
+		return self::$_calleesuffix;
+	}
+	
+	public static function setCalleepath($path)
+	{
+		self::$_calleepath = $path;
+	}
+	
+	public static function getCalleepath()
+	{
+		return self::$_calleepath;
+	}
+	
 	public static function call($who = '', $params = array())
 	{
 		
