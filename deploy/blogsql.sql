b55d438cf50980542a3b015879d0b8b8e94c10ad
wzy <monkeydady@gmail.com>
2014-11-16 19:43:16 +0800

    博客系统基础代码到此结束
    博客系统基础代码到此结束

---
+++ deploy/blogsql.sql
@@ -0,0 +1,34 @@
+/**
+ * 博客信息表
+ */
+create table bloginfo(
+	title varchar(255) not null default '', 
+	
+);
+
+
+/**
+ * 博文表
+ */
+create table article(
+
+);
+
+
+/**
+ * 用户表
+ */
+create table users(
+
+);
+
+
+/**
+ * 评论表
+ */
+create table comment(
+
+);
+
+
+
--- library/Session.php
+++ library/Session.php
@@ -1,5 +1,34 @@
 <?php
+/**
+ * session类
+ * @author wzy
+ */
 class Session
 {
+	/**
+	 * session start
+	 */
+	public static function start()
+	{
+		session_start();
+	}
 	
+	/**
+	 * get session
+	 */
+	public static function get($key)
+	{
+		if (isset($_SESSION[$key])) {
+			return $_SESSION[$key];
+		}
+		return null;
+	}
+	
+	/**
+	 * destroy session
+	 */
+	public static function destroy()
+	{
+		session_destroy();
+	}	
 }
